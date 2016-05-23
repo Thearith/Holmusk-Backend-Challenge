fe520f795741836ef759b3ad2936c8f7790a1d1e
thearith <sothearith.sreang@gmail.com>
2016-05-24 04:23:44 +0800

    adding Food model

---
+++ Holmusk-Daily/api/models/Food.js
@@ -0,0 +1,92 @@
+/**
+ * Food.js
+ *
+ * @description :: Model of food (including title, link, and its nutrient details)
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    title: {
+      type: 'string'
+    },
+
+    link: {
+      type: 'string'
+    },
+
+    serving: {
+      type: 'string'
+    },
+
+    calories: {
+      type: 'string'
+    },
+
+    totalFat: {
+      type: 'string'
+    },
+
+    saturated: {
+      type: 'string'
+    },
+
+    polyunsaturated: {
+      type: 'string'
+    },
+
+    monounsaturated: {
+      type: 'string'
+    },
+
+    trans: {
+      type: 'string'
+    },
+
+    cholesterol: {
+      type: 'string'
+    },
+
+    sodium: {
+      type: 'string'
+    },
+
+    potassium: {
+      type: 'string'
+    },
+
+    totalCarbs: {
+      type: 'string'
+    },
+
+    dietaryFiber: {
+      type: 'string'
+    },
+
+    sugars: {
+      type: 'string'
+    },
+
+    protein: {
+      type: 'string'
+    },
+
+    vitaminA: {
+      type: 'string'
+    },
+
+    vitaminC: {
+      type: 'string'
+    },
+
+    calcium: {
+      type: 'string'
+    },
+
+    iron: {
+      type: 'string'
+    }
+  }
+
+};
