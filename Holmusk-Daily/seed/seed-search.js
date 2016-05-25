493bc9a1c55ab1513b80648b8de1684cd9f93ba3
thearith <sothearith.sreang@gmail.com>
2016-05-25 18:05:52 +0800

    fix seed-search to account for failed post requests

--- Holmusk-Daily/seed/seed-search.js
+++ Holmusk-Daily/seed/seed-search.js
@@ -1,44 +1,89 @@
+// import libraries
 var scrape = require('../scrape/scrape');
 var elasticsearch = require('../search/elasticsearch');
 var rp = require('request-promise');
+var helper = require('../helper/helper');
 
 // constants
 var HOLMUSK_DAILY_BASE_URL  = "http://localhost:1337";
-var GET_FOOD_ID_ENDPOINT    = HOLMUSK_DAILY_BASE_URL + "/foodTitle/";
+var GET_FOOD_ID_ENDPOINT    = HOLMUSK_DAILY_BASE_URL + "/foodHash/";
+
+
+// variables
+var foodJSONList;
+var numFoodSavedToSearchList;
+
 
 elasticsearch.indexExists()
   .then(function (exists) {
     if (exists) {
-      console.log("deleted");
+      console.log("\nDeleted all elasticsearch data\n");
       return elasticsearch.deleteIndex();
     }
   })
   .then(function () {
-    return elasticsearch.initIndex().then(elasticsearch.initMapping)
+    return elasticsearch.initIndex()
+      .then(elasticsearch.initMapping)
       .then(function() {
         scrape(function(foodsJSON) {
-          foodsJSON.forEach(function(foodJSON, index) {
-
-            url = encodeURI(GET_FOOD_ID_ENDPOINT + foodJSON.title);
-
-            var options = {
-              uri: url,
-              json: true
-            };
-
-            rp(options)
-              .then(function(res) {
-                foodJSON.id = res.id;
-                console.log(res.id);
-                elasticsearch.addFood(foodJSON);
-              })
-              .catch(function(err) {
-                console.log(err);
-              })
-          });
-        })
-
-        console.log("\n\nAdded " + foodsJSON.length + " foods to elasticsearch client\n");
-
+          init(foodsJSON);
+          saveFoodListToElasticSearch(callback);
+        });
       });
   });
+
+var init = function(foodsJSON) {
+  console.log("\n");
+
+  foodJSONList = foodsJSON;
+  numFoodSavedToSearchList = 0;
+
+  for(i=0; i<foodJSONList.length; i++)
+    foodJSONList[i].traversed = false;
+}
+
+var saveFoodListToElasticSearch = function(callback) {
+  var count = 0;
+  var totalFoodNeeded = foodJSONList.length - numFoodSavedToSearchList;
+
+  foodJSONList.forEach(function(foodJSON, index) {
+
+    if(!foodJSON.traversed) {
+      hash = helper.hashCode(foodJSON.title);
+      url = encodeURI(GET_FOOD_ID_ENDPOINT + hash);
+
+      var options = {
+        uri: url,
+        json: true
+      };
+
+      rp(options)
+        .then(function(res) {
+          foodJSON.id = res.id;
+          elasticsearch.addFood(foodJSON);
+
+          foodJSON.traversed = true;
+          numFoodSavedToSearchList++;
+
+          count++;
+          if(count > totalFoodNeeded - 1)
+            callback();
+        })
+        .catch(function(err) {
+          count++;
+          if(count > totalFoodNeeded - 1)
+            callback();
+        })
+      }
+  });
+}
+
+var callback = function() {
+  console.log("Saved " + numFoodSavedToSearchList + " foods to elasticsearch client");
+
+  if(numFoodSavedToSearchList > foodJSONList.length - 1) {
+    console.log("\nSuccessfully Added " + foodJSONList.length + " foods to elasticsearch client\n");
+  } else {
+    saveFoodListToElasticSearch(callback);
+  }
+}
