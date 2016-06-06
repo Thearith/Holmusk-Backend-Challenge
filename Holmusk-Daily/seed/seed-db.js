97b3ec3c2522825a95c80529260d9ad1cd0ac73c
thearith <sothearith.sreang@gmail.com>
2016-06-07 00:20:22 +0800

    write seed-db and seed-search with Promises and ES6 syntax

--- Holmusk-Daily/seed/seed-db.js
+++ Holmusk-Daily/seed/seed-db.js
@@ -6,31 +6,45 @@
 *******************************************************************************/
 
 // Import libraries
-var scrape = require('../scrape/scrape.js');
-var rp = require('request-promise');
-
+import RequestPromise from 'request-promise';
+import scrapeFoods from '../scrape/scrape.js';
 
 // Constants
-var HOLMUSK_DAILY_BASE_URL  = "http://localhost:1337";
-var POSTING_ENDPOINT        = HOLMUSK_DAILY_BASE_URL + "/foods";
+const LOCALHOST               = 'http://localhost:1337';
+const HOSTED_URL              = 'http://holmusk-daily-63927.onmodulus.net/';
+const POSTING_ENDPOINT        = '/foods';
+
+
+
+/*
+**********************************************************************
+************************** Export functions **************************
+**********************************************************************
+*/
 
 
 // First, scrape foods using scrape.js which will return a list of foods, foodsJSON
 // After that, make a POST request to store foodsJSON
 
-scrape(function(foodsJSON) {
-  var options = {
-    method: 'POST',
-    uri: POSTING_ENDPOINT,
-    body: foodsJSON,
-    json: true
-  };
+const url = process.env.NODE_ENV === 'production'
+  ? HOSTED_URL + POSTING_ENDPOINT
+  : LOCALHOST + POSTING_ENDPOINT;
 
-  rp(options)
-    .then(function(parsedBody){
-      console.log("\n" + parsedBody.msg + "\n"); // success
+export default function seedDatabase() {
+  scrapeFoods()
+    .then((foodsJSON) => {
+      const options = {
+        method: 'POST',
+        uri: url,
+        body: foodsJSON,
+        json: true
+      };
+
+      RequestPromise(options)
+        .then((parsedBody) => console.log("\n" + parsedBody.msg + "\n"))
+        .catch(err => reject(err));
     })
-    .catch(function(err) {
-      // deal with excpetions
-    })
-})
+    .catch(err => console.log(err));
+}
+
+seedDatabase();
--- Holmusk-Daily/seed/seed-search.js
+++ Holmusk-Daily/seed/seed-search.js
@@ -7,110 +7,158 @@
 *******************************************************************************/
 
 
-// import libraries
-var scrape = require('../scrape/scrape');
-var elasticsearch = require('../search/elasticsearch');
-var rp = require('request-promise');
-var helper = require('../helper/helper');
+// Import Libraries
+import RequestPromise from 'request-promise';
+import scrapeFoods from '../scrape/scrape';
+import elasticSearch from '../search/elasticSearch';
+import getHashCode from '../helper/helper';
 
 
-// constants
-var HOLMUSK_DAILY_BASE_URL  = "http://localhost:1337";
-var GET_FOOD_ID_ENDPOINT    = HOLMUSK_DAILY_BASE_URL + "/foodHash/";
+// Constants
+const LOCALHOST               = "http://localhost:1337";
+const HOSTED_URL              = 'http://holmusk-daily-63927.onmodulus.net/';
+const GET_FOOD_ID_ENDPOINT    = "/foodHash/";
 
-
-// variables
-var foodJSONList;
-var numFoodSavedToSearchList;
+const FOOD_ID_ENDPOINT = process.env.NODE_ENV === 'production'
+  ? HOSTED_URL + GET_FOOD_ID_ENDPOINT
+  : LOCALHOST + GET_FOOD_ID_ENDPOINT;
 
 
 // First, if elasticsearch contains existing data, erase all of them
 // Second, initialize elasticsearch mapping (more info in /search/search.js)
 // Third, scrape foods using scrape.js which will return a list of foods, foodsJSON
-// Finally, post the list of scraped foods to elasticsearch db
 
-elasticsearch.indexExists()
-  .then(function (exists) {
-    if (exists) {
-      console.log("\nDeleted all elasticsearch data\n");
-      return elasticsearch.deleteIndex();
-    }
-  })
-  .then(function () {
-    return elasticsearch.initIndex()
-      .then(elasticsearch.initMapping)
-      .then(function() {
-        scrape(function(foodsJSON) {
-          init(foodsJSON);
-          saveFoodListToElasticSearch(callback);
-        });
+function initializeElasticSearch() {
+  return new Promise((resolve) => {
+    elasticSearch.indexExists()
+      .then((exists) => {
+        if (exists) {
+          console.log("\nDeleted all elasticsearch data\n");
+          return elasticsearch.deleteIndex();
+        }
+      })
+      .then(function () {
+        return elasticsearch.initIndex()
+          .then(elasticsearch.initMapping)
+          .then(() => resolve());
       });
   });
-
-
-// init function before posting the list of foods to elasticsearch
-
-var init = function(foodsJSON) {
-  console.log("\n");
-
-  foodJSONList = foodsJSON;
-  numFoodSavedToSearchList = 0;
-
-  for(i=0; i<foodJSONList.length; i++)
-    foodJSONList[i].traversed = false;
 }
 
 
-// save the list of food to elasticsearch server
-// callback is called after traversing through all foods
+function saveFoodToElasticSearch(food) {
 
-var saveFoodListToElasticSearch = function(callback) {
-  var count = 0;
-  var totalFoodNeeded = foodJSONList.length - numFoodSavedToSearchList;
+  return new Promise((resolve, reject) => {
+    const hash = getHashCode(food.title);
+    const url = encodeURI(FOOD_ID_ENDPOINT + hash);
 
-  foodJSONList.forEach(function(foodJSON, index) {
+    const options = {
+       uri: url,
+      json: true
+    };
 
-    if(!foodJSON.traversed) {
-      hash = helper.hashCode(foodJSON.title);
-      url = encodeURI(GET_FOOD_ID_ENDPOINT + hash);
+    RequestPromise(options)
+      .then((res) => {
+        food.id = res.id;
+        elasticsearch.addFood(food);
+        resolve();
+      })
+      .catch((err) => {
+        //TODO: redo if failed
+      })
+  });
+}
 
-      var options = {
-        uri: url,
-        json: true
-      };
-
-      rp(options)
-        .then(function(res) {
-          foodJSON.id = res.id;
-          elasticsearch.addFood(foodJSON);
-
-          foodJSON.traversed = true;
-          numFoodSavedToSearchList++;
-
-          count++;
-          if(count > totalFoodNeeded - 1)
-            callback();
-        })
-        .catch(function(err) {
-          count++;
-          if(count > totalFoodNeeded - 1)
-            callback();
-        })
-      }
+function saveFoodsToElasticSearch(foods) {
+  return new Promise((resolve, reject) => {
+    Promise
+      .all(foods.map(food => saveFoodToElasticSearch(food)))
+      .then(() => {
+        console.log("\nSuccessfully Added " + foods.length + " foods to elasticsearch client\n");
+      })
+      .catch(err => reject(err));
   });
 }
 
 
-// callback is called after saveFoodListToElasticSearch() finishes execution
-//    If some foods still have not been saved to the db (bad requests, caught at execution),
-//    saveFoodListToElasticSearch() will be called again until all foods have saved to db.
+/*
+**********************************************************************
+************************** Export functions **************************
+**********************************************************************
+*/
 
-var callback = function() {
-  console.log("Saved " + numFoodSavedToSearchList + " foods to elasticsearch client");
-
-  if(numFoodSavedToSearchList > foodJSONList.length - 1) {
-    console.log("\nSuccessfully Added " + foodJSONList.length + " foods to elasticsearch client\n");
-  } else {
-    saveFoodListToElasticSearch(callback);
-  }
+export default function seedElasticSearch() {
+  initializeElasticSearch()
+    .then(() => scrapeFoods())
+    .then((foods) => saveFoodsToElasticSearch(foods))
+    .catch(err => console.log(err));
 }
+
+seedElasticSearch();
+
+// // init function before posting the list of foods to elasticsearch
+
+// var init = function(foodsJSON) {
+//   console.log("\n");
+
+//   foodJSONList = foodsJSON;
+//   numFoodSavedToSearchList = 0;
+
+//   for(i=0; i<foodJSONList.length; i++)
+//     foodJSONList[i].traversed = false;
+// }
+
+
+// // save the list of food to elasticsearch server
+// // callback is called after traversing through all foods
+
+// var saveFoodListToElasticSearch = function(callback) {
+//   var count = 0;
+//   var totalFoodNeeded = foodJSONList.length - numFoodSavedToSearchList;
+
+//   foodJSONList.forEach(function(foodJSON, index) {
+
+//     if(!foodJSON.traversed) {
+//       hash = helper.hashCode(foodJSON.title);
+//       url = encodeURI(GET_FOOD_ID_ENDPOINT + hash);
+
+//       var options = {
+//         uri: url,
+//         json: true
+//       };
+
+//       rp(options)
+//         .then(function(res) {
+//           foodJSON.id = res.id;
+//           elasticsearch.addFood(foodJSON);
+
+//           foodJSON.traversed = true;
+//           numFoodSavedToSearchList++;
+
+//           count++;
+//           if(count > totalFoodNeeded - 1)
+//             callback();
+//         })
+//         .catch(function(err) {
+//           count++;
+//           if(count > totalFoodNeeded - 1)
+//             callback();
+//         })
+//       }
+//   });
+// }
+
+
+// // callback is called after saveFoodListToElasticSearch() finishes execution
+// //    If some foods still have not been saved to the db (bad requests, caught at execution),
+// //    saveFoodListToElasticSearch() will be called again until all foods have saved to db.
+
+// var callback = function() {
+//   console.log("Saved " + numFoodSavedToSearchList + " foods to elasticsearch client");
+
+//   if(numFoodSavedToSearchList > foodJSONList.length - 1) {
+//     console.log("\nSuccessfully Added " + foodJSONList.length + " foods to elasticsearch client\n");
+//   } else {
+//     saveFoodListToElasticSearch(callback);
+//   }
+// }
